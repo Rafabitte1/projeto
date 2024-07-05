20d23d19e0cb1771e811430ab43f5c6502b49704
Rafabitte1 <rafabitte1@gmail.com>
2024-07-04 21:58:12 -0300

    alteração da cor

--- src/App.vue
+++ src/App.vue
@@ -127,7 +127,7 @@ const submitForm = () => {
   background-color: #fff;
   padding: 20px;
   border-radius: 8px;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  box-shadow: 0 0 10px rgba(185, 179, 179, 0.1);
 }
 
 h1 {
